c42dd29107fc2f0b663223c388d45640337099de
MahafuzulHuq <gmsanzid@gmail.com>
2026-05-14 10:58:06 +0600

    Readme Add

--- WebAppDockerize/obj/Debug/net9.0/WebAppDockerize.AssemblyInfo.cs
+++ WebAppDockerize/obj/Debug/net9.0/WebAppDockerize.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppDockerize")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a683b00afada074c28f75a58471abe157890f34c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppDockerize")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppDockerize")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
